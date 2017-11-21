b0eff64472af090c1f6df76fe98f6bb73a6d24e3
Johan Rodin <johan.n.rodin@se.ibm.com>
2017-11-21 15:53:58 +0000

    text

--- js/handleRequest.js
+++ js/handleRequest.js
@@ -15,7 +15,7 @@ function onTextClick() {
 			//$('#id_contextdump').prepend(createnewText('Response from Watson: ' + response.output.text));
 			$('#id_contextdump').prepend(createnewText(JSON.stringify(response.output.text, null, 2)));
         	$('#id_contextdump').show();
-        	$('#conversation_output').val(JSON.stringify(response, null, 2));
+        	$('#conversation_output').prepend(createnewText(JSON.stringify(response, null, 2)));
         }
            
     	function processNotOK(err) {
