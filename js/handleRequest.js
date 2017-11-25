f4c2d85f553db5fb55810037c5181cdfdead2432
Johan Rodin <johan.n.rodin@se.ibm.com>
2017-11-25 13:11:25 +0000

    table

--- js/handleRequest.js
+++ js/handleRequest.js
@@ -10,7 +10,7 @@ function onTextClick() {
 			
 		}*/
 		function createnewBar(score) {
-			return $(`<div id=\"bar\" class=\"segments load\"><div class=\"from-watson top\"><div class=\"message-inner\"><div class=\"column\"><progress class=\"progress progress-good is-medium\" value=\"${score}\" max=\"100\"></progress>${score}</div></div></div></div>`);			
+			return $(`<div id=\"bar\" class=\"segments load\"><div class=\"from-watson top\"><div class=\"message-inner\"><div class=\"column\"><progress class=\"progress progress-good is-medium\" value=\"${score}\" max=\"100\">${score}</progress></div></div></div></div>`);			
 		}
 		function createnewText(who, text){
 			if (who === 'Bot') {
@@ -37,6 +37,7 @@ function onTextClick() {
 			return $('<pre></pre>').html(text);
 		}
 		function processOK(response) {
+			var the_score = 1;
 			console.log('OK');
 			$('#loading').hide();
 			//$('#id_contextdump').prepend(createnewText('Response from Watson: ' + response.output.text));
@@ -44,7 +45,9 @@ function onTextClick() {
 			//save context
 			$('#usercontext').val(JSON.stringify(response.context, null, 2));
 			$('#id_contextdump').prepend(createnewText('Bot', response.output.text[0]));
-			$('#id_contextdump').prepend(createnewBar(response.intents[0].confidence.toFixed(2)*100));
+			if ('undefined' !== typeof response.intents[0].confidence)
+				the_score = (Math.round((response.intents[0].confidence * 1000)/10)/100).toFixed(2);
+			$('#id_contextdump').prepend(createnewBar(the_score*100));
         	$('#id_contextdump').show();
         	//$('#conversation_output').prepend(createnewTextPre(formatJSON(JSON.stringify(response, null, 2), false)));
         	$('#conversation_output').prepend(createnewTextPre(JSON.stringify(response, null, 2), false));       	
