2aada5e797d0884a0186fa7e867b72ae4b2ba34e
Johan Rodin <johan.n.rodin@se.ibm.com>
2017-11-25 14:38:42 +0000

    toggle

--- js/toggle.js
+++ js/toggle.js
@@ -1,4 +1,10 @@
+/*eslint-disable no-undef, no-unused-vars*/
 function toggle(id) {
     var state=document.getElementById(id).style.display;
-    document.getElementById(id).style.display=(state == 'block' ? 'none' : 'block');
+    document.getElementById(id).style.display=state === 'block' ? 'none' : 'block';
+}
+
+function toggleClass(tgclass) {
+	var state=jQuery(tgclass).style.display;
+    jQuery(tgclass).style.display=state === 'block' ? 'none' : 'block';
 }
