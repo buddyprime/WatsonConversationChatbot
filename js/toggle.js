bdbd4906bb9ebdd88b0f9fd47f1c8c823cafd0ae
Johan Rodin <johan.n.rodin@se.ibm.com>
2017-11-26 09:22:50 +0000

    new toggle class

--- js/toggle.js
+++ js/toggle.js
@@ -6,8 +6,22 @@ function toggle(id) {
 
 function toggleClass(tgclass) {
 	var els = document.getElementsByClassName(tgclass);
-    for(var i=0; i<els.length; ++i){
-		var s = els[i].style;
-      	s.display = s.display==='none' ? 'block' : 'none';
-   }
+	var currentDisplay = 'none';
+	
+	//semi toggle, set ALL to display=none xor set ALL to display=block
+	currentDisplay = els[0].style.display;
+	
+	if (currentDisplay === 'none') {
+	    for(var i=0; i<els.length; ++i){
+			var s = els[i].style;
+      		//s.display = s.display==='none' ? 'block' : 'none';
+      		s.display = 'block';
+   		}
+   	} else {
+   		for(var j=0; j<els.length; ++j){
+			var ss = els[j].style;
+      		//s.display = s.display==='none' ? 'block' : 'none';
+      		ss.display = 'none';		
+   		}
+	}
 }
